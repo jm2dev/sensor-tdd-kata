710936528405dcbc9dd23352939796b6a07db055
jm2dev <jomarcar@gmail.com>
2012-12-08 00:33:59 +0000

    Refactoring the implementation in a more scala-ish way.

--- src/main/scala/Sensor.scala
+++ src/main/scala/Sensor.scala
@@ -7,6 +7,9 @@ case class Sensor(rawdata: List[Int])
   def numberOfResults = rawdata.head
 
   def dataAt(x: Int, y: Int): Int = {
+    val raw = rawdata.tail.tail
+    def transform(x: Int, y: Int): Int = x * gridSize + y
+
     if ( x >= 0 && y >= 0 && x < gridSize & y < gridSize )
       raw( transform(x, y) )
     else
@@ -33,25 +36,17 @@ case class Sensor(rawdata: List[Int])
   def uniqueScoringPoints: List[Int] = scoringPoints.toSet.toList.sorted.reverse
 
   def results: String = {
+    def extractScore(lista: List[Int]): String = {
+      def antitransform(i: Int): (Int, Int) = (i / gridSize, i % gridSize)
+
+      val score = lista.head
+      val candidate = scoringPoints.indexOf(score)
+      val coordinates = antitransform(candidate)
+      "(%s, %s score: %s)".format(coordinates._1, coordinates._2, score)
+    }
+
     (1 to numberOfResults).map( (i:Int) =>
       extractScore(uniqueScoringPoints.drop(i - 1))
     ).toSet.mkString("")
-  }
-
-  private def extractScore(lista: List[Int]): String = {
-    val score = lista.head
-    val candidate = scoringPoints.indexOf(score)
-    val coordinates = antitransform(candidate)
-    "(%s, %s score: %s)".format(coordinates._1, coordinates._2, score)
-  }
-
-  private val raw = rawdata.tail.tail
-
-  private def transform(x: Int, y: Int): Int = {
-    x * gridSize + y
-  }
-
-  private def antitransform(i: Int): (Int, Int) = {
-    (i / gridSize, i % gridSize)
   }
 }
